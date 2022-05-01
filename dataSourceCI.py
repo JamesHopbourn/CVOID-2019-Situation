eb00f75c3af4059fcc4e544345a2d8c98f3343cc
JamesHopbourn <hopbourncontact@gmail.com>
2022-05-01 11:20:08 +0800

    dataSourceCI.py
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- dataSourceCI.py
+++ dataSourceCI.py
@@ -8,7 +8,13 @@ from datetime import date
 
 disable_warnings()
 today = str(date.today())
-token = os.environ['token']
+plus_token = os.environ['plus_token']
+github_token = os.environ['repo_token']
+
+name = "James Hopbourn"
+email = "jameshopbourn@gmail.com"
+dataSQLpath = f'https://api.github.com/repos/shortcutstemp/test/contents/data.sql'
+todaySQLpath = f'https://api.github.com/repos/shortcutstemp/test/contents/SQLInsert/{today}.sql'
 
 resp = requests.get("https://ncov.dxy.cn/ncovh5/view/pneumonia", verify=False)
 data = re.findall(r"(?<=window.getAreaStat =).*?(?=}catch)", str(resp.content, "utf-8"))
@@ -16,15 +22,51 @@ data = json.loads(data[0])
 
 execute = "INSERT INTO detailCount (date, provinceName, currentConfirmedCount, confirmedCount, deadCount, curedCount) VALUES\n"
 for i in range(len(data)):
-    execute += str((today,data[i]["provinceName"],data[i]["currentConfirmedCount"],data[i]["confirmedCount"],data[i]["deadCount"],data[i]["curedCount"]))
+    execute += str((today, data[i]["provinceName"], data[i]["currentConfirmedCount"], data[i]["confirmedCount"],
+                    data[i]["deadCount"], data[i]["curedCount"]))
     execute += ',\n'
-    
-byte_encoder = base64.b64encode(execute[:-2].encode("utf-8"))
-string_encoder = byte_encoder.decode('utf-8')
 
-response = requests.put(
-    url=f'https://api.github.com/repos/:owner/:repo/contents/:path/{today}.sql',
-    headers={"Authorization": f"token {token}"},
-    data=f'{{"message": "Data upload", "content":"{string_encoder}", "committer":{{"name":"NAME","email":"EMAIL"}}}}', verify=False
+
+# 报错推送
+def statusCheck(funcName, statusCode):
+    if (funcName == "创建文件" and statusCode == 422):
+        return
+    if (statusCode < 200 or statusCode >= 300):
+        content = f'{{"token": "{plus_token}", "title":"出错啦~", "content" : "{funcName}:{statusCode}", "template": "json"}}'
+        update = requests.put(
+            url='https://www.pushplus.plus/send',
+            data=content.encode('utf-8'), verify=False
+        )
+
+
+# 获取 prev today.sql
+get = requests.get(
+    url=dataSQLpath,
+    headers={"Authorization": f"token {github_token}"}, verify=False
 )
-print(response.status_code)
+statusCheck("获取文件", get.status_code)
+prev_sha = json.loads(get.text)['sha']
+prev_data = json.loads(get.text)['content']
+prev_data = base64.b64decode(prev_data).decode('utf-8')
+comb_text = f'{prev_data}\n\n{execute[:-2]};'
+comb_data = base64.b64encode(comb_text.encode("utf-8")).decode('utf-8')
+
+# 更新 dataSQL.sql
+if (today not in prev_data):
+    update = requests.put(
+        url=dataSQLpath,
+        headers={"Authorization": f"token {github_token}"},
+        data=f'{{"message": "Data update", "content":"{comb_data}", "sha" : "{prev_sha}",\
+        "committer":{{"name":"{name}","email":"{email}"}}}}', verify=False
+    )
+    statusCheck("更新文件", update.status_code)
+
+# 创建当日SQL文件
+today_data = base64.b64encode(f"{execute[:-2]};".encode("utf-8")).decode('utf-8')
+upload = requests.put(
+    url=todaySQLpath,
+    headers={"Authorization": f"token {github_token}"},
+    data=f'{{"message": "Data upload", "content":"{today_data}", \
+    "committer":{{"name":"{name}","email":"{email}"}}}}', verify=False
+)
+statusCheck("创建文件", upload.status_code)
