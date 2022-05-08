c6ca07a6f5c1b4c636b3c3669d2fd384740ba348
林钰婷 <1192600535@qq.com>
2022-05-08 14:26:29 +0800

    添加小组成员:林钰婷

---
+++ src/main/java/com/yutty/cvoid2019situation/Cvoid2019SituationApplication.java
@@ -0,0 +1,23 @@
+package com.yutty.cvoid2019situation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Slf4j
+@SpringBootApplication
+@ServletComponentScan
+@EnableTransactionManagement
+@EnableCaching //开启SpringCache注解方式的缓存
+public class Cvoid2019SituationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cvoid2019SituationApplication.class, args);
+        log.info("项目启动成功...");
+    }
+
+}
