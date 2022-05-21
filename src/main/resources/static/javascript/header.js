537f3e99e0ca43a64adc28c7c8364864c72ea0c9
JamesHopbourn <hopbourncontact@gmail.com>
2022-05-21 20:11:11 +0800

    header HTML 代码封装
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

---
+++ src/main/resources/static/javascript/header.js
@@ -0,0 +1,10 @@
+document.getElementById("app-header").innerHTML = `
+    <div>
+        <ul>
+            <li><a class="a1" href="index.html">首页</a></li>
+            <li class="rightBar"><a class="a2" href="knowledge.html">防疫科学小知识</a></li>
+            <li class="rightBar"><a class="a3" href="notice.html">疫苗接种注意事项</a></li>
+            <li class="rightBar"><a class="a3" href="https://github.com/JamesHopbourn/CVOID-2019-Situation">GitHub 项目仓库</a></li>
+        </ul>
+    </div>
+`;
