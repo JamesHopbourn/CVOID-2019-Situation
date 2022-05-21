cb4574afa47fb877ccb245bcbbe62a91b11c745a
JamesHopbourn <hopbourncontact@gmail.com>
2022-05-21 17:54:32 +0800

    删除 Java 多余的注释
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- src/main/java/com/yutty/cvoid2019situation/Cvoid2019SituationApplication.java
+++ src/main/java/com/yutty/cvoid2019situation/Cvoid2019SituationApplication.java
@@ -4,19 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
 
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
-@EnableTransactionManagement              
-@EnableCaching
 public class Cvoid2019SituationApplication {
     public static void main(String[] args) {
         SpringApplication.run(Cvoid2019SituationApplication.class, args);
         log.info("项目启动成功...");
     }
-
 }
