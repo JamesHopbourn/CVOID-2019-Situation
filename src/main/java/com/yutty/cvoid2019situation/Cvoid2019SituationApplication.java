1431002405e1e61460898b5c3e4e8c692732a1af
陈志烨 <1915304878@qq.com>
2022-05-22 23:25:48 +0800

    启动项目时自动打开主页

--- src/main/java/com/yutty/cvoid2019situation/Cvoid2019SituationApplication.java
+++ src/main/java/com/yutty/cvoid2019situation/Cvoid2019SituationApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 @Slf4j
 @SpringBootApplication
 @ServletComponentScan
@@ -12,5 +17,24 @@ public class Cvoid2019SituationApplication {
     public static void main(String[] args) {
         SpringApplication.run(Cvoid2019SituationApplication.class, args);
         log.info("项目启动成功...");
+        browse("http://localhost:8089/index.html");
+    }
+
+    public static void browse(String url) {
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("rundll32 url.dll,FileProtocolHandler " + url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
